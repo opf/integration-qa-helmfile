baecff5f33bfadfd7ac983ceabbb78b21e577eec
Saw-jan <saw.jan.grg3e@gmail.com>
2025-10-28 15:14:29 +0545

    fix: add token-exchange grant type
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- charts/opnc-integration/scripts/openproject/setup-op-source.sh
+++ charts/opnc-integration/scripts/openproject/setup-op-source.sh
@@ -49,6 +49,11 @@ cp ./config/database.production.yml ./config/database.yml
 sed -i 's/ --configuration production --named-chunks --source-map//' ./frontend/package.json
 DOCKER=0 NG_CLI_ANALYTICS="false" bash ./docker/prod/setup/precompile-assets.sh
 
+if [[ -n "$OP_GIT_SOURCE_BRANCH" ]] && [[ "$OP_USE_LOCAL_SOURCE" != "true" ]]; then
+    sed -i 's|rm -f ./config/database.yml||' ./docker/prod/setup/postinstall-common.sh
+    rm -rf "$APP_PATH/tmp"
+fi
+
 # set sticky bit on app path and tmp directory
 chmod +t "$APP_PATH"
 chmod +t "/tmp"
