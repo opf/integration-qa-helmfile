464bcf37ab63ac61c35fa56b4e289b868b0dd89f
Saw-jan <saw.jan.grg3e@gmail.com>
2026-03-25 15:24:55 +0545

    feat: add test setup configs
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- charts/opnc-integration/scripts/openproject/setup-op-source.sh
+++ charts/opnc-integration/scripts/openproject/setup-op-source.sh
@@ -36,31 +36,13 @@ fi
 
 # trust git repos
 git config --global safe.directory '*'
-# database config
-rm -f ./config/database.yml
-cp ./config/database.production.yml ./config/database.yml
 
-if [[ "$OP_USE_LOCAL_SOURCE" == "true" ]]; then
-    sed -i 's/production:/development:/' ./config/database.yml
-    export BUNDLE_APP_CONFIG=./.bundle
-    export BUNDLE_WITHOUT=""
-    bundle config set --local path './vendor/bundle'
-    bundle config set --local with 'development test'
-    bundle install
-    npm install
-    SECRET_KEY_BASE=1 RAILS_ENV=development DATABASE_URL=nulldb://db \
-        bin/rails openproject:plugins:register_frontend assets:export_locales
-else
-    bash ./docker/prod/setup/bundle-install.sh
-    # remove source map and production optimizations from build to speed up build time
-    sed -i 's/ --configuration production --named-chunks --source-map//' ./frontend/package.json
-    DOCKER=0 NG_CLI_ANALYTICS="false" bash ./docker/prod/setup/precompile-assets.sh
-fi
+cp /scripts/database.yaml ./config/database.yml
+bin/setup_dev
+bin/rails db:seed db:migrate db:test:prepare
+bin/rails openproject:plugins:register_frontend assets:export_locales
 
-if [[ -n "$OP_GIT_SOURCE_BRANCH" ]] && [[ "$OP_USE_LOCAL_SOURCE" != "true" ]]; then
-    sed -i 's|rm -f ./config/database.yml||' ./docker/prod/setup/postinstall-common.sh
-    rm -rf "$APP_PATH/tmp"
-fi
+rm -rf "$APP_PATH/tmp"
 
 chown "$APP_USER":"$APP_USER" -R "$APP_PATH"
 # set sticky bit on app path and tmp directory
