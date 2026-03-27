edd5e3be5a7c3733260b1719679a537820e69996
Saw-jan <saw.jan.grg3e@gmail.com>
2026-03-27 11:50:40 +0545

    fix: prevent test setup in production mode
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- charts/opnc-integration/scripts/openproject/setup-op-source.sh
+++ charts/opnc-integration/scripts/openproject/setup-op-source.sh
@@ -12,33 +12,31 @@ fi
 
 set -x
 
-# install psql client
-apt update > /dev/null && apt install -y postgresql-client > /dev/null
-# install nodejs 22
-curl -skL https://nodejs.org/dist/latest-v22.x/node-v22.22.2-linux-x64.tar.gz | tar -xz -C /usr/local --strip-components=1
-
 mkdir -p "$APP_PATH" && cd "$APP_PATH"
 
-if [[ -n $(find "$APP_PATH" -mindepth 1 -print -quit) ]] && [[ "$OP_USE_LOCAL_SOURCE" != "true" ]]; then
-    echo "[ERROR] '$APP_PATH' is not empty. Please delete the volume and try again."
-    exit 1
-fi
-
-if [[ -n "$OP_GIT_SOURCE_BRANCH" ]] && [[ "$OP_USE_LOCAL_SOURCE" != "true" ]]; then
+if [[ -n "$OP_GIT_SOURCE_BRANCH" ]] && [[ "$OP_USE_LOCAL_SOURCE" != "true" ]] && [[ -z $(find "$APP_PATH" -mindepth 1 -print -quit) ]]; then
     echo "[INFO] Cloning OpenProject from branch: $OP_GIT_SOURCE_BRANCH"
     git clone --branch "$OP_GIT_SOURCE_BRANCH" --depth 1 --single-branch "https://github.com/opf/openproject" "$APP_PATH"
 fi
 
 cp /scripts/database.yaml ./config/database.yml
 
-bundle config set --local path './vendor/bundle'
-bundle config set --local with 'development test'
+export SECRET_KEY_BASE=1
+rails_with=""
+db_cmd="db:seed"
+if [[ "$RAILS_ENV" != "production" ]]; then
+    rails_with="development test"
+    db_cmd="$db_cmd db:test:prepare"
+fi
+
+bundle config set --local path "./vendor/bundle"
+bundle config set --local with "$rails_with"
 
 # wait for database to be ready
 timeout 300s bash -c "until psql $DATABASE_URL -c '\q'; do echo 'Waiting for database...'; sleep 2; done"
 
 bin/setup_dev
-bin/rails db:seed db:test:prepare
+bin/rails $db_cmd
 
 chown "$APP_USER":"$APP_USER" -R "$APP_PATH"
 # set sticky bit on app path and tmp directory
