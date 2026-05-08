e7eb8ca9111a01044cac5b6d9cbfac96a7d7de9c
Ihor Khomenko <39989281+Ihor-Khomenko@users.noreply.github.com>
2026-05-08 14:46:21 +0300

    Add Helm repository configuration in pre-script for dependency management

--- pullpreview/pre-script-helm-deps.sh
+++ pullpreview/pre-script-helm-deps.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 echo "[pullpreview pre_script] Building Helm dependencies on the instance..."
 cd /app
+echo "[pullpreview pre_script] Ensuring Helm repos are configured..."
+
+helm repo add --force-update nextcloud https://nextcloud.github.io/helm
+helm repo add --force-update bitnami https://charts.bitnami.com/bitnami
+helm repo add --force-update traefik https://traefik.github.io/charts
+
+helm repo update
+
 helm dependency build charts/pullpreview-stack
 echo "[pullpreview pre_script] Done."
 
